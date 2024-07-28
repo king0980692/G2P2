8e917191041309d4a373b01e35bdee05ff4548d6
Leon Chang <king0980692@gmail.com>
2024-07-28 15:19:57 +0800

    build env script

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+pip install torch==2.1
+pip install numpy==1.26.4
+
+pip install torch_geometric
+pip install torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.1.0+cpu.html
+
+pip install tqdm pandas ftfy regex
+pip install faiss-cpu loguru
